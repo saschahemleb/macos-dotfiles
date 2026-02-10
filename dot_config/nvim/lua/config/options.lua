e79026ceccffdafa503fe6f5a52e76a9e4bfbdf6
Sascha Hemleb <sascha.hemleb@eventim.de>
2026-02-10 13:27:17 +0100

    feat: remove lazyvim setup
    Signed-off-by: Sascha Hemleb <sascha.hemleb@eventim.de>

--- dot_config/nvim/lua/config/options.lua
+++
@@ -1,5 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: -- Add any additional options here
-
-vim.opt.showmode = false
-vim.opt.wrap = true
